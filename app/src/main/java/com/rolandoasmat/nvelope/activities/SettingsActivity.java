9ffabea5e9bd3881c013bdba45e20d2159094a63
Rolando Asmat <rolandoasmat@gmail.com>
2017-09-02 01:57:54 -0500

    Settings page, ability to delete a category

--- app/src/main/java/com/rolandoasmat/nvelope/activities/SettingsActivity.java
+++ app/src/main/java/com/rolandoasmat/nvelope/activities/SettingsActivity.java
@@ -1,23 +1,110 @@
 package com.rolandoasmat.nvelope.activities;
 
+import android.content.DialogInterface;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v4.app.LoaderManager.LoaderCallbacks;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
 
 import com.rolandoasmat.nvelope.R;
+import com.rolandoasmat.nvelope.adapters.CategoriesAdapter;
+import com.rolandoasmat.nvelope.models.CategoriesTable;
+import com.rolandoasmat.nvelope.models.Category;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity implements LoaderCallbacks<Cursor>, CategoriesAdapter.CategoryClickListener {
     @BindView(R.id.categories_recycler_view)
     protected RecyclerView mCategoriesRecyclerView;
     protected CategoriesAdapter mAdapter;
+
+    private final int FETCH_CATEGORIES = 4949;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         ButterKnife.bind(this);
+
+        setupRecyclerView();
+    }
+
+    private void setupRecyclerView() {
+        mAdapter = new CategoriesAdapter(this);
+        mCategoriesRecyclerView.setAdapter(mAdapter);
+        mCategoriesRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    private void refreshUI() {
+        getSupportLoaderManager().initLoader(FETCH_CATEGORIES, null, this);
+    }
+
+    private void bindRecyclerView(Cursor cursor) {
+        List<Category> receipts = CategoriesTable.getRows(cursor, false);
+        mAdapter.updateData(receipts);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        refreshUI();
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        switch(i) {
+            case FETCH_CATEGORIES:
+                return new CursorLoader(this, CategoriesTable.CONTENT_URI, null, null, null, null);
+            default:
+                throw new IllegalArgumentException("ID: " + i + " not handled.");
+        }
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        int id = loader.getId();
+        switch(id) {
+            case FETCH_CATEGORIES:
+                bindRecyclerView(cursor);
+                break;
+            default:
+                throw new IllegalArgumentException("ID: " + id + " not handled.");
+        }
+        getSupportLoaderManager().destroyLoader(id);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) { }
+
+    @Override
+    public void onClick(final String category) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setMessage("Delete " +category+ " category?");
+        // Add the buttons
+        builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked OK button
+                getContentResolver().delete(CategoriesTable.CONTENT_URI,"name == ?", new String[]{category});
+                refreshUI();
+            }
+        });
+        builder.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User cancelled the dialog
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
--- app/src/main/java/com/rolandoasmat/nvelope/adapters/CategoriesAdapter.java
+++ app/src/main/java/com/rolandoasmat/nvelope/adapters/CategoriesAdapter.java
@@ -4,10 +4,11 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.rolandoasmat.nvelope.R;
-import com.rolandoasmat.nvelope.models.Receipt;
+import com.rolandoasmat.nvelope.models.Category;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,59 +17,68 @@ import java.util.List;
  * Created by rolandoasmat on 9/2/17.
  */
 
-public class CategoriesAdapter  extends RecyclerView.Adapter<ReceiptEntryViewHolder> {
+public class CategoriesAdapter  extends RecyclerView.Adapter<CategoriesAdapter.CategoryEntryViewHolder> {
 
-    private List<Receipt> mCategories;
-    public CategoriesAdapter(List<Receipt> receipts) {
-        mCategories = receipts;
+    private List<Category> mCategories;
+    private final CategoryClickListener mClickHandler;
+
+    public interface CategoryClickListener {
+        void onClick(String category);
     }
-    public CategoriesAdapter() {
+
+    public CategoriesAdapter(CategoryClickListener onClickListener) {
         mCategories = new ArrayList<>();
+        mClickHandler = onClickListener;
     }
 
-    public void updateData(List<Receipt> receipts){
-        this.mCategories = receipts;
+    public void updateData(List<Category> categories){
+        this.mCategories = categories;
         notifyDataSetChanged();
     }
 
     @Override
-    public ReceiptEntryViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public CategoryEntryViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.receipt_list_item, parent, false);
-        return new ReceiptEntryViewHolder(view);
+                .inflate(R.layout.category_list_item, parent, false);
+        return new CategoryEntryViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(ReceiptEntryViewHolder holder, int position) {
-        Receipt receipt = mCategories.get(position);
-        holder.bind(receipt);
+    public void onBindViewHolder(CategoryEntryViewHolder holder, int position) {
+        Category category = mCategories.get(position);
+        holder.bind(category);
     }
 
     @Override
     public int getItemCount() {
         return mCategories.size();
     }
+
+    class CategoryEntryViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public TextView mLabel;
+        public ImageButton mButton;
+
+        public CategoryEntryViewHolder(View itemView) {
+            super(itemView);
+            mLabel = getTextView(itemView, R.id.category_label);
+            mButton = itemView.findViewById(R.id.delete_image_button);
+            mButton.setOnClickListener(this);
+        }
+        @Override
+        public void onClick(View v) {
+            int adapterPosition = getAdapterPosition();
+            String category = mCategories.get(adapterPosition).mName;
+            mClickHandler.onClick(category);
+        }
+
+        private TextView getTextView(View view, int id) {
+            return (TextView) view.findViewById(id);
+        }
+
+        public void bind(Category category) {
+            mLabel.setText(category.mName);
+        }
+    }
 }
 
-class CategoryEntryViewHolder extends RecyclerView.ViewHolder {
-    public TextView mLocation;
-    public TextView mDate;
-    public TextView mAmount;
 
-    public CategoryEntryViewHolder(View itemView) {
-        super(itemView);
-        mLocation = getTextView(itemView, R.id.location_text_view);
-        mDate = getTextView(itemView, R.id.date_text_view);
-        mAmount = getTextView(itemView, R.id.amount_text_view);
-    }
-
-    private TextView getTextView(View view, int id) {
-        return (TextView) view.findViewById(id);
-    }
-
-    public void bind(Receipt receipt) {
-        mLocation.setText(receipt.mLocation);
-        mDate.setText(receipt.dateFormatted());
-        mAmount.setText( Double.toString(receipt.mAmount));
-    }
-}
